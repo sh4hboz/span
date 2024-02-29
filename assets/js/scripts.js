cb8a0fe4394a25751448689198ad90a1d37dab89
shahboz <suyuunov.shaxboz2704@gmail.com>
2024-02-29 19:20:11 +0500

    edit close img

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -56,7 +56,7 @@ $(document).ready(function () {
 		});
 	}
 
-	$(".hamburger , .close_menu svg").click(function () {
+	$(".hamburger , .close_menu p").click(function () {
 		$(".menus").toggleClass("active");
 		$(".has_submenu, .has_submenu li ").removeClass("active");
 	});
